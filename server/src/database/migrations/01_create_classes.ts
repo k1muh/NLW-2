d66a711cecfe3adce204e93655a2010905774caa
k1muh <36715909+kimihkim@users.noreply.github.com>
2020-10-31 08:57:23 -0300

    Final

---
+++ server/src/database/migrations/01_create_classes.ts
@@ -0,0 +1,22 @@
+import Knex from 'knex';
+
+// Criar tabela
+export async function up(knex: Knex) {
+    return knex.schema.createTable('classes', table => {
+        table.increments('id').primary();
+        table.string('subject').notNullable();
+        table.decimal('cost').notNullable();
+
+        table.integer('user_id')
+            .notNullable()
+            .references('id')
+            .inTable('users')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE');
+    });
+}
+
+// Deletar tabela
+export async function down(knex: Knex) {
+    return knex.schema.dropTable('classes');
+}
